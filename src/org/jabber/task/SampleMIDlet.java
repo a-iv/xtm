ad693be4ffbc885f973c283aa793cc77bd3a3cbd
Igor tarasov <igdmi.tar@googlewave.com>
2010-01-26 21:39:40 +0500

    Первая попытка подружиться с XMPP

--- src/org/jabber/task/SampleMIDlet.java
+++ src/org/jabber/task/SampleMIDlet.java
@@ -1,5 +1,8 @@
 package org.jabber.task;
 
+import java.io.IOException;
+import java.util.Vector;
+
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
@@ -41,7 +44,11 @@ import javax.microedition.rms.RecordStoreNotOpenException;
  */
 import javax.microedition.rms.RecordStoreNotFoundException;
 
-public class SampleMIDlet extends MIDlet implements CommandListener {
+import net.sourceforge.jxa.Jxa;
+import net.sourceforge.jxa.XmppListener;
+import net.sourceforge.jxa.client.Roster;
+
+public class SampleMIDlet extends MIDlet implements CommandListener, XmppListener {
 	int k = 0;
 
 	String recordStoreName = "user1";
@@ -61,9 +68,10 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 	RecordStore recordStore = null;
 	List sort;
 
-	// 1 строка - имя
-	// 2 строка - работа
-	// 3 строка - пароль
+	Jxa jxa;
+	Image offlineImg;
+	Image onlineImg;
+	//private Roster roster = new Roster();
 
 	TextField pass = new TextField("Введите пароль", "", 20, TextField.PASSWORD);
 	TextField password = new TextField("Пароль", "", 20, TextField.PASSWORD);
@@ -79,6 +87,7 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 	TextField topic2 = new TextField("Тема:", "", 160, 0);
 	TextField descript2 = new TextField("Описание:", "", 1024, 0);
 	TextField userJID = new TextField("JID:", "", 1024, 0);
+	TextField serv = new TextField("server", "", 128, 0);
 
 	Gauge gauge = new Gauge("Выполнено", true, 10, 0);
 	// gauge.getValue()
@@ -102,34 +111,9 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 	Command cSort = new Command("Сортировка", Command.ITEM, 5);
 	String State;
 
-	public class User {
-		String jid = new String();
-		String password = new String();
-		String name = new String();
-		String post = new String();
-	}
-
-	User[] users = new User[32];
-	int usersCount = 4;
-
-	User getUserByJID(String jid) {
-		for (int i = 0; i < usersCount; i++) {
-			if (users[i].jid == jid)
-				return users[i];
-		}
-		return null;
-	}
-
-	public class Contact {
-		String owner = new String();
-		String jid = new String();
-		String name = new String();
-		String post = new String();
-	}
-
-	Contact[] contacts = new Contact[32];
-	int contactsCount = 6;
-
+	Vector contacts = new Vector();
+	Vector tasks = new Vector();
+/*	
 	Task[] tasks = new Task[200];
 	int tasksCount = 4;
 
@@ -141,6 +125,7 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 		return null;
 	}
 
+*/
 	Comment[] comments = new Comment[2500];
 	int commentsCount = 0;
 
@@ -153,19 +138,19 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 	void printContacts() {
 		formContacts.deleteAll();
 		int k = 0;
-		for (int i = 0; i < contactsCount; i++)
-			if (contacts[i].owner == thisUserJID) {
-				formContacts.append(contacts[i].name, null);
-				printedContactIDs[k] = contacts[i].jid;
-				k++;
+		for (int i = 0; i < contacts.size(); i++) {
+			Contact contact = (Contact) contacts.elementAt(i); 
+			System.out.println(contact.name);
+			formContacts.append(contact.name, null);
 			}
 	}
 
-	void onCreateContact(String jid, String name, String post) {
-		contacts[contactsCount] = new Contact();
-		contacts[contactsCount].jid = jid;
-		contacts[contactsCount].name = name;
-		contacts[contactsCount].post = post;
+	void onCreateContact(String jid, String name, String group) {
+		Contact contact = new Contact();
+		contact.jid = jid;
+		contact.name = name;
+		contact.group = group;
+		contacts.addElement(contact);
 	}
 
 	void createContact(String jid, String name, String post) {
@@ -173,17 +158,15 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 		printContacts();
 	}
 
-	void deleteContact(String id) {
+	void deleteContact(String jid) {
 		int index = 0;
-		for (int i = 0; i < contactsCount; i = i + 1) {
-			if (contacts[i].jid == id) {
+		for (int i = 0; i < contacts.size(); i = i + 1) {
+			Contact contact = (Contact) contacts.elementAt(i);
+			if (contact.jid == jid) {
 				index = i;
 			}
 		}
-		for (int i = index; i < contactsCount; i++) {
-			contacts[i] = contacts[i + 1];
-		}
-		contactsCount = contactsCount - 1;
+		contacts.removeElementAt(index);
 		printContacts();
 	}
 
@@ -324,59 +307,23 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 	}
 
 	protected void startApp() throws MIDletStateChangeException {
+		onlineImg = loadImage("/online.png");
+		offlineImg = loadImage("/offline.png");
+
 		display = Display.getDisplay(this);
 
 		// тестер класов
-		users[0] = new User();
-		users[0].name = "1";
-		users[0].jid = "1a";
-		users[0].password = "a1";
-		users[0].post = "aa";
-		users[1] = new User();
-		users[1].name = "2";
-		users[1].jid = "2b";
-		users[1].password = "b2";
-		users[1].post = "bb";
-		users[2] = new User();
-		users[2].name = "3";
-		users[2].jid = "3c";
-		users[2].password = "c3";
-		users[2].post = "cc";
-		users[3] = new User();
-		users[3].name = "4";
-		users[3].jid = "4d";
-		users[3].password = "d4";
-		users[3].post = "dd";
-		contacts[0] = new Contact();
-		contacts[0].owner = "1a";
-		contacts[0].jid = "2b";
-		contacts[0].name = "2";
-		contacts[0].post = "bb";
-		contacts[1] = new Contact();
-		contacts[1].owner = "1a";
-		contacts[1].jid = "3c";
-		contacts[1].name = "3";
-		contacts[1].post = "cc";
-		contacts[2] = new Contact();
-		contacts[2].owner = "2b";
-		contacts[2].jid = "1a";
-		contacts[2].name = "1";
-		contacts[2].post = "aa";
-		contacts[3] = new Contact();
-		contacts[3].owner = "2b";
-		contacts[3].jid = "4d";
-		contacts[3].name = "4";
-		contacts[3].post = "dd";
-		contacts[4] = new Contact();
-		contacts[4].owner = "3c";
-		contacts[4].jid = "1a";
-		contacts[4].name = "1";
-		contacts[4].post = "aa";
-		contacts[5] = new Contact();
-		contacts[5].owner = "4d";
-		contacts[5].jid = "2b";
-		contacts[5].name = "2";
-		contacts[5].post = "bb";
+		Contact contact;
+		contact = new Contact();
+		contact.jid = "2b";
+		contact.name = "2";
+		contact.group = "bb";
+		contacts.addElement(contact);
+		contact = new Contact();
+		contact.jid = "3c";
+		contact.name = "3";
+		contact.group = "cc";
+		contacts.addElement(contact);
 		tasks[0] = new Task(0, "1a", "2b", "1aTo2B", "from1a", 0);
 		tasks[1] = new Task(1, "2b", "1a", "2bTo1a", "from2b", 4);
 		tasks[2] = new Task(2, "3c", "1a", "3cTo1a", "from3c", 6);
@@ -388,6 +335,7 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 		login.setCommandListener(this);
 		login.append(userJID);
 		login.append(pass);
+		login.append(serv);
 
 		// главное меню
 		main = new List("Главное меню", Choice.IMPLICIT);
@@ -447,7 +395,6 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 		// подключение
 		connection.setCommandListener(this);
 		connection.append("подключение к серверу");
-		connection.addCommand(select);
 		connection.addCommand(exit);
 
 		// инфо о контакте
@@ -493,7 +440,9 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 		if (getRecordText(2) != null) {
 			pass.setString(getRecordText(2));
 		}
-
+		if (getRecordText(3) != null) {
+			serv.setString(getRecordText(3));
+		}
 	}
 
 	String getRecordText(int id) {
@@ -541,34 +490,20 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 		if (display.getCurrent() == login) {
 			// Форма входа
 			if (c == select) {
-				String pw = new String();
-				int k = 0;
-				for (int i = 0; i < usersCount; i = i + 1) {
-					if (userJID.getString().equals(users[i].jid)) {
-						pw = users[i].password;
-						thisUserJID = users[i].jid;
-					}
-				}
-				if (pw.equals(pass.getString())) {
-					setRecordText(1, userJID.getString().getBytes());
-					setRecordText(2, pass.getString().getBytes());
-					display.setCurrent(connection);
-				}
-				if (!pw.equals(pass.getString())) {
-					login.append("Неверный парроль");
-					k = k + 1;
-				}
-				if (!pw.equals(pass.getString()) && k == 3) {
-					destroyApp(true);
-				}
-			} else if (c == exit) {
+			setRecordText(1, userJID.getString().getBytes());
+			setRecordText(2, pass.getString().getBytes());
+			setRecordText(3, serv.getString().getBytes());
+			//display.setCurrent(connection);
+			display.setCurrent(main);
+/*					jxa = new Jxa(thisUserJID, pw, "mobile", 10, server, "5223", true);
+					jxa.addListener(this);
+					jxa.start();*/
+				} else if (c == exit) {
 				destroyApp(true);
 			}
 		} else if (display.getCurrent() == connection) {
 			// подключение
-			if (c == select) {
-				display.setCurrent(main);
-			} else if (c == exit) {
+			if (c == exit) {
 				destroyApp(true);
 			}
 		} else if (display.getCurrent() == main) {
@@ -582,13 +517,8 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 					printContacts();
 					break;
 				case 1:
-					display.setCurrent(ychet);
-					for (int i = 0; i < usersCount; i++) {
-						if (thisUserJID == users[i].jid) {
-							name.setString(users[i].name);
-							work.setString(users[i].post);
-							password.setString(users[i].password);
-						}
+					display.setCurrent(ychet);{
+						
 					}
 					break;
 				case 2:
@@ -626,14 +556,9 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 				a = formContacts.getSelectedIndex();
 				display.setCurrent(contactinfo);
 				contactinfo.deleteAll();
-				String s = printedContactIDs[a];
-				for (int i = 0; i < 32; i++) {
-					if (users[i].jid.equals(s)) {
-						contactinfo.append("Имя:  " + users[i].name);
-						contactinfo.append("Должность:  " + users[i].post);
-					}
-				}
-			} else if (c == open) {
+				contactinfo.append("Имя:  " + ((Contact) contacts.elementAt(a)).name);
+				contactinfo.append("Должность:  " + ((Contact) contacts.elementAt(a)).group);
+				} else if (c == open) {
 				thisContactJID = printedContactIDs[formContacts
 						.getSelectedIndex()];
 				String contname = formContacts.getString(formContacts
@@ -665,12 +590,9 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 				display.setCurrent(formContacts);
 				printContacts();
 			} else if (c == ok && !newContactJID.getString().equals("")) {
-				for (int i = 0; i < usersCount; i++) {
-					if (newContactJID.getString().equals(users[i].jid)) {
-						onCreateContact(users[i].jid, users[i].name,
-								users[i].post);
-					}
-				}
+				Contact contact = new Contact();
+				contact.name = newContactJID.getString();
+				contact.jid = newContactJID.getString();
 				display.setCurrent(formContacts);
 				printContacts();
 			}
@@ -689,27 +611,20 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 			if (c == back) {
 				display.setCurrent(main);
 			} else if (c == ok && !name.getString().equals("")) {
-				String s = name.getString();
+				//String s = name.getString();
 				/*
 				 * byte[] data = s.getBytes(); setRecordText(1,data);
 				 */
-				String s2 = work.getString();
+				//String s2 = work.getString();
 				/*
 				 * byte[] data2 = s2.getBytes(); setRecordText(2,data2);/
 				 */
-				String s3 = password.getString();
+				//String s3 = password.getString();
 				/*
 				 * byte[] data3 = s3.getBytes(); setRecordText(3, data3);
 				 */
-				for (int i = 0; i < 32; i++) {
-					if (users[i].jid == thisUserJID) {
-						users[i].name = s;
-						users[i].password = s3;
-						users[i].post = s2;
-					}
-				}
-				display.setCurrent(main);
 			}
+				display.setCurrent(main);
 		} else if (display.getCurrent() == todeletec) {
 			if (c == no) {
 				display.setCurrent(formContacts);
@@ -780,5 +695,75 @@ public class SampleMIDlet extends MIDlet implements CommandListener {
 				}
 			}
 		}
+	}
+	
+	private Image loadImage(String fileName) {
+		Image image = null;
+		try {
+			image = Image.createImage(fileName);
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		}
+		return image;
+	}
+
+
+	public void onAuth(String resource) {
+		try {
+			jxa.getRoster();
+		} catch (final IOException ex) {
+			ex.printStackTrace();
+		}
+		display.setCurrent(main);
+	}
+
+	public void onAuthFailed(String message) {
+		// TODO Auto-generated method stub
+		System.out.println("Авторизация не удалась");
+	}
+
+	public void onConnFailed(String msg) {
+		// TODO Auto-generated method stub
+		System.out.println("Подключение не удалось");
+	}
+
+	public void onContactEvent(final String jid, String name,final String group,
+			final String subscription) {
+		if (subscription.equals("both"))
+			formContacts.append(name, offlineImg);
+			Contact contact = new Contact();
+			contact.jid = jid;
+			contact.name =name;
+			contacts.addElement(contact);
+			}
+
+	public void onContactOverEvent() {
+	}
+
+	public void onMessageEvent(String from, String body) {
+	}
+
+	public void onStatusEvent(String jid, String show, String status) {
+	}
+
+	public void onSubscribeEvent(String jid) {
+		// TODO Auto-generated method stub
+		System.out.println("Subscribe from " + jid);
+		jxa.subscribe(jid);
+	}
+
+	public void onTaskEvent(Task task) {
+		// TODO Auto-generated method stub
+		/*
+		thisTask = new Alert("Task " + task.sender, task.description, null, AlertType.INFO);
+		thisTask.setTimeout(Alert.FOREVER);
+		Display.getDisplay(this).setCurrent(thisTask);
+		*/
+	}
+
+	public void onUnsubscribeEvent(String jid) {
+		// TODO Auto-generated method stub
+		System.out.println("Unsubscribe from " + jid);
+		jxa.unsubscribe(jid);
 	}
 }
